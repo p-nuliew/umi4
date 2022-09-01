67420b240d21b423d022f43fbd2a49366f8ff073
chenweilun <13566106399@163.com>
2022-09-01 14:21:44 +0800

    feat: add styles/themes

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,9 @@
 // 配置文件
+import Themes from './src/styles/themes/templates';
 export default {
   npmClient: 'pnpm',
-  plugins: ['@umijs/plugins/dist/locale'],  // 单独安装@umijs/plugins后，启用 i18n 插件
+  title: 'Aleo Pool',
+  plugins: ['@umijs/plugins/dist/locale', '@umijs/plugins/dist/antd'],  // 单独安装@umijs/plugins后，启用 i18n、antd 插件
   locale: {
     antd: true, // 如果项目依赖中包含 `antd`，则默认为 true
     baseNavigator: true,
@@ -9,6 +11,17 @@ export default {
     default: 'zh-TW', // 默认使用 src/locales/zh-TW.ts 作为多语言文件
     title: false,
     useLocalStorage: true,
+  },
+  antd: {
+    // configProvider
+    configProvider: {},
+    // themes
+    dark: false,
+    compact: false,
+    // babel-plugin-import
+    import: true,
+    // less or css, default less
+    style: 'less',
   },
   history: {
     type: 'hash'
@@ -22,4 +35,7 @@ export default {
     { path: '/index', component: 'index' },
     { path: '/docs', component: 'docs' },
   ],
+  // Theme for antd
+  // https://ant.design/docs/react/customize-theme
+  theme: Themes.light,
 };
